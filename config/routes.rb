6d6e318ddab03f2e846e06d3ecf0647c7a7d2596
Ural Sadritdinov <ural.sadritdinov@flatstack.com>
2022-02-21 16:19:50 +0300

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :home, only: :index
+  root "home#index"
 end
