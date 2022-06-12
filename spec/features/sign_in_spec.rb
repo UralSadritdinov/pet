726ffe9e306333e577ec3be1feb45b0850f67f8a
Ural Sadritdinov <ural.sadritdinov@flatstack.com>
2022-06-13 01:19:15 +0300

    style

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,21 +2,21 @@ require "rails_helper"
 
 feature "Signing in" do
   background do
-    User.create(email: 'user@example.com', password: 'caplin')
+    User.create(email: "user@example.com", password: "caplin")
   end
 
   scenario "Signing in with correct credentials" do
-    visit '/users/sign_in'
+    visit "/users/sign_in"
 
     within("#email") do
-      fill_in 'Email', with: 'user@example.com'
+      fill_in "Email", with: "user@example.com"
     end
 
     within("#password") do
-      fill_in 'Password', with: 'caplin'
+      fill_in "Password", with: "caplin"
     end
 
-    click_button 'Sign in'
+    click_button "Sign in"
     expect(page).to have_current_path(root_path)
   end
 end
